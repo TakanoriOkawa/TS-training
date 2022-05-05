35c4ad0e1f6434f4e4cd4338a33e0b29823a9640
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-06 01:51:44 +0900

    Q1追加　optinalに変換

--- src/section1.ts
+++ src/section1.ts
@@ -1,2 +1,13 @@
-let message: string = 'Hello TypeScr';
-console.log({ message });
+// Q1 Fooが持つプロパティ全てoptionalにする
+
+interface Foo {
+  bar: string;
+  baz: number;
+}
+// TypeScript特有の組み込み型関数
+// Partial
+// （参考）https://log.pocka.io/ja/posts/typescript-builtin-type-functions/
+console.log('Q1🍰');
+
+// 答え
+type PartialFoo = Partial<Foo>;
